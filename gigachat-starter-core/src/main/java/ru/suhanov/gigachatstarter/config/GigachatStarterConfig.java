c2b753dbd39e11b751960a802dca74205bcd79d0
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2025-03-26 20:49:50 +0300

    new

--- gigachat-starter-core/src/main/java/ru/suhanov/gigachatstarter/config/GigachatStarterConfig.java
+++ gigachat-starter-core/src/main/java/ru/suhanov/gigachatstarter/config/GigachatStarterConfig.java
@@ -2,7 +2,6 @@ package ru.suhanov.gigachatstarter.config;
 
 
 import okhttp3.OkHttpClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,48 +9,25 @@ import org.springframework.context.annotation.Configuration;
 import ru.suhanov.gigachatstarter.gigachatapimapper.tooldesc.prop.ToolProperty;
 import ru.suhanov.gigachatstarter.secretholder.fromproperty.AiProperties;
 import ru.suhanov.gigachatstarter.sending.client.prop.SenderProp;
-import org.springframework.core.io.Resource;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
 
 @Configuration
 @EnableConfigurationProperties({AiProperties.class, SenderProp.class, ToolProperty.class})
 @ComponentScan("ru.suhanov.gigachatstarter")
 public class GigachatStarterConfig {
-    @Value("${webclient.ssl.ca-bundle-file}") // Путь к CA-сертификату из конфигурации
-    private Resource caBundleFile;
 
     @Bean
-    public OkHttpClient okHttpClient() throws Exception {
-        // Загрузка CA-сертификата
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        Certificate caCertificate;
-        try (InputStream caBundleStream = caBundleFile.getInputStream()) {
-            caCertificate = certificateFactory.generateCertificate(caBundleStream);
-        }
+    public OkHttpClient okHttpClient(SenderProp senderProp) throws Exception {
+        SenderProp.SenderCertProp certProp = senderProp.getSenderCertProp();
+        OkHttpClient.Builder okBuilder = new OkHttpClient.Builder()
+                .callTimeout(senderProp.getTimeout())
+                .connectTimeout(senderProp.getTimeout())
+                .readTimeout(senderProp.getTimeout())
+                .writeTimeout(senderProp.getTimeout());
 
-        // Создание KeyStore и добавление CA-сертификата
-        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        keyStore.load(null, null); // Инициализация пустого KeyStore
-        keyStore.setCertificateEntry("caCert", caCertificate);
+        boolean hasCertFile = certProp.getCertFilePath() != null;
+        boolean hasKeyCert = certProp.getKeyFilePath() != null;
+        boolean hasCaBundleCert = certProp.getCaBundleFile() != null;
 
-        // Создание TrustManagerFactory
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-
-        // Создание SSLContext
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-
-        // Создание OkHttpClient с настроенным SSLContext
-        return new OkHttpClient.Builder()
-                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustManagerFactory.getTrustManagers()[0])
-                .build();
+        return null;
     }
 }
--- gigachat-starter-core/src/main/java/ru/suhanov/gigachatstarter/sending/client/prop/SenderProp.java
+++ gigachat-starter-core/src/main/java/ru/suhanov/gigachatstarter/sending/client/prop/SenderProp.java
@@ -1,21 +1,28 @@
 package ru.suhanov.gigachatstarter.sending.client.prop;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
 
 @Data
 @ConfigurationProperties("sender-client")
 public class SenderProp {
 
     private SenderCheckProp senderCheckProp = new SenderCheckProp();
+    private SenderCertProp senderCertProp = new SenderCertProp();
+    private Duration timeout = Duration.ofMinutes(1);
 
     @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
     public static class SenderCheckProp {
         private Boolean checkStatus = true;
-        public Integer successStatus = 200;
+        private Integer successStatus = 200;
+    }
+
+    @Data
+    public static class SenderCertProp {
+        private String certFilePath;
+        private String keyFilePath;
+        private String caBundleFile;
     }
 }
